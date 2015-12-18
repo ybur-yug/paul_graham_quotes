fd93830e9b2e3f409d56f349c3011f6801b9bd9f
Ybur Yug <bobbygrayson@gmail.com>
2015-12-18 16:02:11 -0400

    lolol

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,7 +4,7 @@ class QuotesController < ApplicationController
   # GET /quotes
   # GET /quotes.json
   def index
-    @quotes = Quote.all
+    @quotes = Quote.all.limit(100)
   end
 
   # GET /quotes/1
