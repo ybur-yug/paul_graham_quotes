65bcdd15da35be16c0deaf82a27440cc72dd0437
Ybur Yug <bobbygrayson@gmail.com>
2015-12-19 16:54:40 -0400

    lololololol

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,11 @@
+require 'marky'
+
 class Quote < ActiveRecord::Base
+  def self.markov_quote
+    Quote.create(body: PaulGrahamQuote.new.gen_quote)
+  end
+
+  def self.generate_n_markov_quotes number_of_quotes
+    number_of_quotes.times { self.markov_quote }
+  end
 end
--- lib/marky.rb
+++ lib/marky.rb
@@ -1,9 +1,13 @@
 require 'marky_markov'
-markov = MarkyMarkov::Dictionary.new('bernie_trump_dictionary') # Saves/opens dictionary.mmd
-markov.parse_file "#{Rails.root}/paul_graham.txt"
 
-250.times do 
-  puts (markov.generate_n_sentences 1) + "\n\n\n"
+class PaulGrahamQuote
+  def initialize
+    @markov = MarkyMarkov::Dictionary.new('bernie_trump_dictionary') # Saves/opens dictionary.mmd
+    @prng   = Random.new
+    @markov.parse_file "#{Rails.root}/paul_graham.txt"
+  end
+
+  def gen_quote(sentence_count = @prng.rand(1..5))
+    @markov.generate_n_sentences sentence_count
+  end
 end
-
-Quote.new(body: markov.generate_n_sentences(Random.new.rand(4)))
